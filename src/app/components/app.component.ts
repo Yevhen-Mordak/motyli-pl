2b83ab1bed62196ac784a70e3761b090b2451dad
Yevhen Mordak <yevhen_mordak@epam.com>
2018-08-20 18:03:23 +0200

    fix path to assets

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Motyli.pl';
+  title: string = 'Motyli.pl';
+  
 }
