95485db843c74fbb1f4245868b02448aa00afa9f
Yevhen Mordak <yevhen_mordak@epam.com>
2018-08-22 13:47:18 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { KontaktComponent } from './components/kontakt/kontakt.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent }, // default is main
+  { path: 'motyli-pl', component: MainComponent }, // default is main
   { path: 'motyle', component: MotyleComponent }, 
   { path: 'salut', component: SalutComponent }, 
   { path: 'farma', component: FarmaComponent },
