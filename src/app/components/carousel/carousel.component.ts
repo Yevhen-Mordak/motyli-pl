a08c7acd4f8827f407cb1bfe84b666f9eb2e527b
Yevhen Mordak <yevhen_mordak@epam.com>
2018-08-30 09:20:40 +0200

    minor changes

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -11,8 +11,6 @@ export class CarouselComponent implements OnInit {
     {image:'assets/images/2.jpg', header: "Second slide label", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
     {image:'assets/images/3.jpg', header: "Third slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."},
     {image:'assets/images/4.jpg', header: "Forth slide label", text: "Nulla vitae elit libero, a pharetra augue mollis interdum."},
-    {image:'assets/images/5.jpg', header: "Fifth slide label", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
-    {image:'assets/images/6.jpg', header: "Sixth slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."},
     {image:'assets/images/7.jpg', header: "Seventh slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."},
   ];
 
