6d832b7cc70e60e5d9bbd050336b7e0b57011fe5
Yevhen Mordak <yevhen_mordak@epam.com>
2018-09-03 13:40:51 +0200

    optimize images for mobile

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
   images: Array<Image> = [
-    {image:'assets/images/1.jpg', header: "First slide label", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
-    {image:'assets/images/2.jpg', header: "Second slide label", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
-    {image:'assets/images/3.jpg', header: "Third slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."},
-    {image:'assets/images/4.jpg', header: "Forth slide label", text: "Nulla vitae elit libero, a pharetra augue mollis interdum."},
-    {image:'assets/images/7.jpg', header: "Seventh slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."},
+    {image:'1.jpg', header: "First slide label", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    {image:'2.jpg', header: "Second slide label", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    {image:'3.jpg', header: "Third slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."},
+    {image:'4.jpg', header: "Forth slide label", text: "Nulla vitae elit libero, a pharetra augue mollis interdum."},
+    {image:'7.jpg', header: "Seventh slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."},
   ];
 
   constructor() { }
@@ -19,6 +19,12 @@ export class CarouselComponent implements OnInit {
   ngOnInit() {
   }
 
+  adaptImage(image: string) {
+    if(window.innerWidth <= 600)
+      return `assets/images/small/${image}`;
+      
+    return `assets/images/${image}`;
+  }
 }
 
 class Image{
